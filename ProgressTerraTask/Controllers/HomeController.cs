9a795bc3cefa8de0952bdf565bab5943bf2adce0
Farhad Mammadov <farhard_group@mail.ru>
2021-09-14 01:25:59 +0400

    Task

---
+++ ProgressTerraTask/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgressTerraTask.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgressTerraTask.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly RGDialogsClients _rgDialogsClients;
+
+        public HomeController(RGDialogsClients rgDialogsClients)
+        {
+            _rgDialogsClients = rgDialogsClients;
+        }
+
+        [HttpGet]
+        public IActionResult GetById([FromQuery] IEnumerable<Guid> Clients)
+        {
+            var model = _rgDialogsClients.Init().Distinct();
+
+            var idClients = model.Select(x => x.IDClient).Distinct();
+
+            var testCount = idClients.Except(Clients).Count();
+            if (testCount == 0)
+            {
+                var lastdialog = model.FirstOrDefault(x =>
+                    x.IDClient == idClients.Last()).IDRGDialog;
+                return Json(lastdialog);
+            }
+            else
+            {
+                return Json(Guid.Empty);
+            }
+        }
+    }
+}
